0b1817c79a40b8fef62c1ac7b3ffb9a00e34f02e
“mouezbk” <mcmouez11111@gmail.com>
2021-04-09 12:09:07 +0100

    Application mobile de gestion d'exploitation technique et du maintenance

---
+++ app/src/main/java/com/example/radiomonastir/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.radiomonastir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void identification(View view) {
+        EditText champ_email = findViewById(R.id.editTextTextEmailAddress);
+        EditText champ_password = findViewById(R.id.editTextTextPassword);
+
+        if (champ_email.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity.this,"Inserer votre email",Toast.LENGTH_LONG).show();
+        }
+        else if (champ_password.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity.this,"Inserer votre mot de passe",Toast.LENGTH_LONG).show();
+        }
+        else {
+            findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    public void inscription(View view) {
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/radiomonastir/SignUpActivity.java
@@ -0,0 +1,48 @@
+package com.example.radiomonastir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void ajoutemembre(View view) {
+        EditText champ_nom = findViewById(R.id.editTextTextPersonName);
+        EditText champ_email = findViewById(R.id.editTextTextEmailAddress2);
+        EditText champ_passwrod = findViewById(R.id.editTextTextPassword2);
+        EditText champ_confirmepassword = findViewById(R.id.editTextTextPassword3);
+
+        if(champ_nom.getText().toString().isEmpty()){
+            Toast.makeText(SignUpActivity.this,"Ecrire Votre nom et prénom",Toast.LENGTH_LONG).show();
+        }
+        else if (champ_email.getText().toString().isEmpty()){
+            Toast.makeText(SignUpActivity.this,"Le champ email est vide",Toast.LENGTH_LONG).show();
+        }
+        else if (champ_passwrod.getText().toString().isEmpty()){
+            Toast.makeText(SignUpActivity.this,"Entrer un mot de passe",Toast.LENGTH_LONG).show();
+        }
+        else if (champ_confirmepassword.getText().toString().isEmpty()){
+            Toast.makeText(SignUpActivity.this,"Champ confirmer est obligatoire",Toast.LENGTH_LONG).show();
+        }
+        else {
+            findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(SignUpActivity.this,ListActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+}
