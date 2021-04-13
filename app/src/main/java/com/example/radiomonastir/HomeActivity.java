eb99a0cb0aa6e9aa56699c9ce455c3bc87b12850
“mouezbk” <mcmouez11111@gmail.com>
2021-04-13 12:06:23 +0100

    update project

--- app/src/main/java/com/example/radiomonastir/HomeActivity.java
+++ app/src/main/java/com/example/radiomonastir/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.radiomonastir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,85 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void affichestudio(View view) {
+        findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ListStudioActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void afficheplanification(View view) {
+        findViewById(R.id.imageButton3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, PlanificationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void affichecellule(View view) {
+        findViewById(R.id.imageButton6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, CelluleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void affichemaintenance(View view) {
+        findViewById(R.id.imageButton7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MaintenanceActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void afficheserveur(View view) {
+        findViewById(R.id.imageButton8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ServeurActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void affichereformer(View view) {
+        findViewById(R.id.imageView4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ReformerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void affichemagasin(View view) {
+        findViewById(R.id.imageView5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MagasinActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void afficheajoutermemebre(View view) {
+        findViewById(R.id.imageView6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AjouterMembreActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
