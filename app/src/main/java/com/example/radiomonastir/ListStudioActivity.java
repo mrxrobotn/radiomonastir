f5dc5cdfad9e5ab68176927074cf773e6daf1a81
“mouezbk” <mcmouez11111@gmail.com>
2021-04-13 13:44:09 +0100

    ajouter listview

--- app/src/main/java/com/example/radiomonastir/ListStudioActivity.java
+++ app/src/main/java/com/example/radiomonastir/ListStudioActivity.java
@@ -3,12 +3,54 @@ package com.example.radiomonastir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ListStudioActivity extends AppCompatActivity {
 
+    ListView listview;
+    Button Addbutton;
+    EditText GetValue;
+    String[] ListElements = new String[] {
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_studio);
+
+
+        listview = (ListView) findViewById(R.id.listView1);
+        Addbutton = (Button) findViewById(R.id.button1);
+        GetValue = (EditText) findViewById(R.id.editText1);
+
+        final List< String > ListElementsArrayList = new ArrayList< String >
+                (Arrays.asList(ListElements));
+
+
+
+        final ArrayAdapter< String > adapter = new ArrayAdapter < String >
+                (ListStudioActivity.this, android.R.layout.simple_list_item_1,
+                        ListElementsArrayList);
+
+        listview.setAdapter(adapter);
+
+        Addbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                ListElementsArrayList.add(GetValue.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
+
 }
