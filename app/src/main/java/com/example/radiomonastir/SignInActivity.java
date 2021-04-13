1b5364ea273154020611eeaeba4ffcde4749515d
“mouezbk” <mcmouez11111@gmail.com>
2021-04-13 10:19:32 +0100

    update project

--- app/src/main/java/com/example/radiomonastir/SignInActivity.java
+++ app/src/main/java/com/example/radiomonastir/SignInActivity.java
@@ -13,7 +13,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
 
     public void identification(View view) {
