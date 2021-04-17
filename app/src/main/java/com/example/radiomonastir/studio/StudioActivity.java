b3998238c19f770ee4840ed166abdfa97b8b630c
“mouezbk” <mcmouez11111@gmail.com>
2021-04-17 23:13:33 +0100

    add forget and show/hide password add StartActivity

--- app/src/main/java/com/example/radiomonastir/studio/StudioActivity.java
+++ app/src/main/java/com/example/radiomonastir/studio/StudioActivity.java
@@ -14,8 +14,11 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.radiomonastir.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,9 +63,52 @@ public class StudioActivity extends AppCompatActivity {
                 addStudio();
             }
         });
-
     }
 
+
+
+
+
+
+    /*
+     * This method show the database data on the listView
+     * */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //attaching value event listener
+        databaseStudios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //clearing the previous artist list
+                studios.clear();
+
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting artist
+                    Studio studio = postSnapshot.getValue(Studio.class);
+                    //adding artist to the list
+                    studios.add(studio);
+                }
+
+                //creating adapter
+                StudioList studioAdapter = new StudioList(StudioActivity.this, studios);
+                //attaching adapter to the listview
+                listViewStudios.setAdapter(studioAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    /*
+     * This method is saving a new artist to the
+     * Firebase Realtime Database
+     * */
     private void addStudio() {
         //getting the values to save
         String number = editTextNumber.getText().toString().trim();
@@ -92,4 +138,8 @@ public class StudioActivity extends AppCompatActivity {
             Toast.makeText(this, "Entrer un numero", Toast.LENGTH_LONG).show();
         }
     }
+
+
+
+
 }
