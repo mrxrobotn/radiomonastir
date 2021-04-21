e35bbb17cdc98359aa2a11b724464739c160ad09
pc <wiemmehrzi@gmail.com>
2021-04-21 10:19:38 +0100

    update

--- app/src/main/java/com/example/radiomonastir/studio/StudioList.java
+++ app/src/main/java/com/example/radiomonastir/studio/StudioList.java
@@ -1,12 +1,16 @@
 package com.example.radiomonastir.studio;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.radiomonastir.HomeAdminActivity;
+import com.example.radiomonastir.PlanificationActivity;
 import com.example.radiomonastir.R;
 
 import java.util.List;
@@ -30,6 +34,14 @@ public class StudioList extends ArrayAdapter<Studio> {
         TextView textViewNumber = (TextView) listViewItem.findViewById(R.id.textViewNumber);
         TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
 
+        LinearLayout listeitem = (LinearLayout) listViewItem.findViewById(R.id.listeitem);
+        listeitem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context , PlanificationActivity.class);
+                context.startActivity(intent);
+            }
+        });
         Studio studio = studios.get(position);
         textViewNumber.setText(studio.getStudioNumber());
         textViewName.setText(studio.getStudioName());
