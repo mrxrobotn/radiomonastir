f2df7708d74b0619169cadcacc56034f966b67d5
pc <wiemmehrzi@gmail.com>
2021-04-23 22:17:18 +0100

    update

--- app/src/main/java/com/example/radiomonastir/Adapters/CelluleAdapter.java
+++ app/src/main/java/com/example/radiomonastir/Adapters/CelluleAdapter.java
@@ -1,11 +1,15 @@
 package com.example.radiomonastir.Adapters;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,6 +18,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.radiomonastir.CelluleEquipementActivity;
 import com.example.radiomonastir.Models.Cellule;
 import com.example.radiomonastir.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -50,7 +56,7 @@ public class CelluleAdapter extends RecyclerView.Adapter<CelluleViewHolder> {
         celluleViewHolder.item_cellule.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                Toast.makeText(context, "setOnLongClickListener", Toast.LENGTH_SHORT).show();
+                showUpdateDeleteDialog(cellule.getCelluleId(),cellule.getCelluleName());
                 return false;
             }
         });
@@ -61,4 +67,57 @@ public class CelluleAdapter extends RecyclerView.Adapter<CelluleViewHolder> {
     public int getItemCount() {
         return celluleList.size();
     }
+
+    private void showUpdateDeleteDialog(String celluleId, String celluleName) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View dialogView = inflater.inflate(R.layout.cellule_dialogue, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextTextPersonName = (EditText) dialogView.findViewById(R.id.editTextTextPersonName);
+        final Button buttonUpdateCellule = (Button) dialogView.findViewById(R.id.buttonUpdateCellule);
+        final Button buttonDeleteCellule = (Button) dialogView.findViewById(R.id.buttonDeleteCellule);
+
+        dialogBuilder.setTitle(celluleName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        buttonUpdateCellule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextTextPersonName.getText().toString().trim();
+                if (!TextUtils.isEmpty(name)) {
+                    updateCellule(celluleId,name);
+                    b.dismiss();
+                }
+            }
+        });
+        buttonDeleteCellule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                deleteCellule(celluleId);
+                b.dismiss();
+            }
+        });
+    }
+
+    private boolean deleteCellule(String id) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("cellules").child(id);
+
+        dR.removeValue();
+        Toast.makeText(context, "Cellule supprimé avec succée", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean updateCellule(String id, String name) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("cellules").child(id);
+
+        Cellule cellule = new Cellule(id, name);
+        dR.setValue(cellule);
+        Toast.makeText(context, "Cellule modifié avec succée", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/example/radiomonastir/Adapters/ServeurAdapter.java
+++ app/src/main/java/com/example/radiomonastir/Adapters/ServeurAdapter.java
@@ -1,19 +1,24 @@
 package com.example.radiomonastir.Adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.radiomonastir.CelluleEquipementActivity;
-import com.example.radiomonastir.Models.Cellule;
 import com.example.radiomonastir.Models.Serveur;
 import com.example.radiomonastir.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -47,7 +52,7 @@ public class ServeurAdapter extends RecyclerView.Adapter<ServeurViewHolder> {
         serveurViewHolder.item_serveur.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                Toast.makeText(context, "setOnLongClickListener", Toast.LENGTH_SHORT).show();
+                showUpdateDeleteDialog(serveur.getServeurId(),serveur.getServeurName());
                 return false;
             }
         });
@@ -57,5 +62,56 @@ public class ServeurAdapter extends RecyclerView.Adapter<ServeurViewHolder> {
     @Override
     public int getItemCount() {
         return serveurList.size();
+    }
+
+    private void showUpdateDeleteDialog(String serveurId, String serveurName) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View dialogView = inflater.inflate(R.layout.serveur_dialogue, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextServeur = (EditText) dialogView.findViewById(R.id.editTextServeur);
+        final Button btnModifierServeur = (Button) dialogView.findViewById(R.id.btnModifierServeur);
+        final Button btnDeleteServeur = (Button) dialogView.findViewById(R.id.btnDeleteServeur);
+
+        dialogBuilder.setTitle(serveurName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        btnModifierServeur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextServeur.getText().toString().trim();
+                if (!TextUtils.isEmpty(name)) {
+                    updateServeur(serveurId,name);
+                    b.dismiss();
+                }
+            }
+        });
+        btnDeleteServeur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                deleteServeur(serveurId);
+                b.dismiss();
+            }
+        });
+    }
+
+    private boolean deleteServeur(String id) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("serveurs").child(id);
+
+        dR.removeValue();
+        Toast.makeText(context, "serveur supprimé avec succée", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean updateServeur(String id, String name) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("serveurs").child(id);
+
+        Serveur serveur = new Serveur(id, name);
+        dR.setValue(serveur);
+        Toast.makeText(context, "serveur modifié avec succée", Toast.LENGTH_LONG).show();
+        return true;
     }
 }
