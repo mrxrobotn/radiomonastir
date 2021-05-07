61af1c9dbc823503ba577a229cd035e60f864bf2
“mouezbk” <mcmouez11111@gmail.com>
2021-05-08 00:02:07 +0100

    ajouter activity_studio_fiche.xml

---
+++ app/src/main/java/com/example/radiomonastir/StudioFicheActivity.java
@@ -0,0 +1,14 @@
+package com.example.radiomonastir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudioFicheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studio_fiche);
+    }
+}
